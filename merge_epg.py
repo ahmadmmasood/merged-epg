38a0ef31805320531ef2b7f45a61c7cff1ae3500
ahmadmmasood <ahmad.masood@yahoo.com>
2026-02-17 12:39:19 -0500

    Update EPG aliases and improve channel matching

--- merge_epg.py
+++ merge_epg.py
@@ -42,13 +42,100 @@ def similar(a, b):
 # ALIASES (Exact raw EPG IDs)
 # -----------------------------
 EPG_ALIASES = {
-    # Add your known mappings here
-    "home.and.garden.television.hd.us2": "HGTV",
-    "5_starmax.hd.east.us2": "5StarMax",
+    # Premium / specialty
+    "5_starmax": "5StarMax",
+    "outermax": "OuterMax",
+    "moviemax": "MovieMax",
+    "thrillermax": "ThrillerMax",
+    "sho×bet": "SHO×BET",
+    # HBO / Cinemax / Showtime aliases
+    "hbo family": "HBO Family",
+    # Starz / Encore
+    "starz encore family": "Starz Encore Family",
+    "starz encore westerns": "Starz Encore Westerns",
+    "starz inblack": "Starz InBlack",
+    "starz kids and family": "Starz Kids & Family",
+    # Local DC/Baltimore
+    "wbaldt": "WBAL-DT",
+    "wdca-dt": "WDCA-DT",
+    "wdcw-dt": "WDCW-DT",
+    "wdvm-sd": "WDVM-SD",
+    "weta kids": "WETA Kids",
+    "weta uk": "WETA UK",
+    "weta-hd": "WETA-HD",
+    "wfdc-dt": "WFDC-DT",
+    "whut": "WHUT",
     "wjla-dt": "WJLA-DT",
+    "wjz 13": "WJZ 13 (CBS Baltimore)",
+    "wmar 2": "WMAR 2 (ABC Baltimore)",
+    "wmpb": "WMPB (PBS Maryland)",
+    "wnuv-dt": "WNUV-DT",
+    "wrc-hd": "WRC-HD",
+    "wttg-dt": "WTTG-DT",
     "wusa-hd": "WUSA-HD",
+    "wzdc": "WZDC",
+    # Other international / specialty
+    "aaj tak": "Aaj Tak",
+    "al ahram tv": "Al Ahram TV",
+    "al hayat tv": "Al Hayat TV",
+    "al masriya tv": "Al Masriya TV",
+    "al nahar tv": "Al Nahar TV",
+    "al jadeed": "Al Jadeed",
+    "altavsn": "AltaVsn",
     "buzzr": "BUZZR",
-    # Add more missing channels here
+    "bloomberg television": "Bloomberg Television",
+    "cbc": "CBC (Egypt)",
+    "crimes": "CRIMES",
+    "cwwnuv": "CWWNUV",
+    "cartoonito": "Cartoonito",
+    "colors tv": "Colors TV",
+    "comcast sportsnet mid atlantic": "Comcast SportsNet Mid-Atlantic",
+    "crime tv": "Crime TV",
+    "dream tv": "Dream TV",
+    "e!": "E!",
+    "fox sports 1": "Fox Sports 1",
+    "fox sports 2": "Fox Sports 2",
+    "fox weather": "Fox Weather",
+    "future tv": "Future TV",
+    "gettv": "GetTV",
+    "hsn": "HSN",
+    "heroes and icons": "Heroes & Icons",
+    "ion plus": "ION Plus",
+    "lbci": "LBCI",
+    "mpt kids": "MPT Kids",
+    "mpt-2": "MPT-2",
+    "mpt-hd": "MPT-HD",
+    "mtv": "MTV",
+    "mtv lebanon": "MTV Lebanon",
+    "mtv live": "MTV Live",
+    "mtv2": "MTV2",
+    "metv": "MeTV",
+    "metro": "Metro",
+    "nbc sports washington": "NBC Sports Washington",
+    "ndtv": "NDTV",
+    "nhk world japan": "NHK World Japan",
+    "newschannel 8": "NewsChannel 8 (WJLA News)",
+    "nick at nite": "Nick at Nite",
+    "nickmusic": "NickMusic",
+    "nile tv international": "Nile TV International",
+    "otv lebanon": "OTV Lebanon",
+    "oxygen": "Oxygen",
+    "republic tv": "Republic TV",
+    "showtime": "Showtime",
+    "showtime family zone": "Showtime Family Zone",
+    "sony tv": "Sony TV",
+    "star plus": "Star Plus",
+    "story television": "Story Television",
+    "sun tv": "Sun TV",
+    "tcm": "TCM",
+    "tlc": "TLC",
+    "tv9": "TV9",
+    "teennick": "TeenNick",
+    "telexitos": "Telexitos",
+    "the movie channel xtra": "The Movie Channel Xtra",
+    "times now": "Times Now",
+    "travel channel": "Travel Channel",
+    "xitos": "XITOS",
 }
 
 # -----------------------------
@@ -77,7 +164,6 @@ def load_epg_sources():
             line = line.strip()
             if line and not line.startswith("#") and line.startswith("http") and line != LOCAL_FEED_URL:
                 sources.append(line)
-    # Local feed is handled separately
     return sources
 
 # -----------------------------
@@ -119,17 +205,13 @@ def parse_xml_stream(content_bytes, master_cleaned, allowed_master_channels, day
             canonical_id = raw_id.lower()
             matched = False
 
-            # Skip non-local channels if local_only=True
-            if local_only and canonical_id not in allowed_master_channels:
+            if local_only and cleaned_display not in allowed_master_channels:
                 elem.clear()
                 continue
 
-            # Exact alias mapping
             if canonical_id in EPG_ALIASES:
                 matched = True
                 channel_id_to_display[canonical_id] = EPG_ALIASES[canonical_id]
-
-            # Exact match to master list
             elif cleaned_display in master_cleaned:
                 matched = True
                 channel_id_to_display[canonical_id] = master_cleaned[cleaned_display]
